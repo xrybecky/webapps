19f912b4425c0ee1360672be30b1a6982cb55c8e
Viktor Rybecký <rybeckyv@gmail.com>
2016-06-03 16:01:26 +0200

    username login + rooms + joining rooms

--- pexeso/app.js
+++ pexeso/app.js
@@ -2,11 +2,14 @@ setEventHandlers = function(){
 	io.socket.on("connection", onSocketConnection);
 };
 
+var openedRooms = new Array();
+
 var jquery = require('jquery');
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/client/index.html');
@@ -17,12 +20,54 @@ app.use('/client', express.static(__dirname + '/client'));
 io.on('connection', function(socket){
 	console.log('user connected');
 	
-	socket.on('message', function(m){
-			console.log('message ' + m);
+	socket.on('new user', function(username){
+		console.log('new user > ' + username);
+		socket.username = username;
+		socket.room = 'default';
+		socket.join(socket.room);
+		io.to(socket.room).emit('avb rooms', openedRooms);
+	});
+	
+	socket.on('new room', function(room){
+		console.log('new room > ' + room + ' from ' + socket.username);
+		socket.leave(socket.room);
+		socket.room = room;
+		socket.join(socket.room);
+		openedRooms.push({roomname:room, num_users: 1});		
+		io.to('default').emit('avb rooms', openedRooms);
+	});
+	
+	socket.on('join room', function(key){
+		var roomname = openedRooms[key].roomname;
+		if(openedRooms[key].num_users < 2){
+			socket.leave(socket.room);
+			socket.room = roomname;
+			socket.join(socket.room);
+			openedRooms[key].num_users++;	
+			io.to(socket.room).emit('room info', "user " + socket.username + " joined");
+			socket.join(socket.username);
+			io.to(socket.username).emit('joined', '1');
+			socket.leave(socket.username);
+			console.log( socket.username + ' joined room > ' + socket.room);
+		}else{		
+			socket.join(socket.username);
+			io.to(socket.username).emit('joined', '-1');
+			socket.leave(socket.username);
+		}
+		io.to('default').emit('avb rooms', openedRooms);
+	});
+	
+	socket.on('avb rooms', function(){
+		io.to(socket.room).emit('avb rooms', openedRooms);
+	});
+			
+	socket.on('message', function(msg){
+		console.log('message ' + msg + ' from ' + socket.username);
+		io.to(socket.room).emit('message', msg);		
 	});
 	
 	socket.on('disconnect', function(){
-		console.log('user disconnected');
+		console.log('user '+ socket.username +'disconnected');
 	});
 });
 
